bdb3d478fd3aaec9500630f9bf188efb420d5450
JuliaSaxS <120261142+JuliaSaxS@users.noreply.github.com>
2023-06-13 21:58:29 +0300

    Added Task 4.

--- Home_Work_Java/Task_4.java
+++ Home_Work_Java/Task_4.java
@@ -8,24 +8,22 @@ package Home_Work_Java;
 
 public class Task_4 {
     public static void main(String[] args) {
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                for (int k = 0; k < 9; k++) {
-                    for (int l = 0; l < 9; l++) {
-                        for (int m = 0; m < 9; m++) {
-                            for (int n = 0; n < 9; n++) {
-                                if ((i * 10 + j) + (k * 10 + l) == (m * 10 + n)) {
-                                    int a = i * 10 + j;
-                                    int b = k * 10 + l;
-                                    int c = m * 10 + n;
-                                    System.out.printf("%d + %d = %d   ", a, b, c);
-                                }
-                            }
-                        }
-                    }
+
+        System.out.print("\n\nЗадано выражение: 2? + ?5 = 69. Восстановить выражение до верного равенства или сообщить, что его нет.");
+        int count = 0;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if (20 + i + j * 10 + 5 == 69) {
+                    count += 1;
+                    System.out.println(count + ") 2" + i + " + " + j + "5" + " = 69");
                 }
             }
         }
+        System.out.println("\n");
+        if (count == 0) {
+            System.out.println("Решений нет\n");
+        }
+
     }
 }
 
