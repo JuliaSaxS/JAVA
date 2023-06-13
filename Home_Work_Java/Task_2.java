61c41a8d4250500e821794542300d85aa8ca4c58
JuliaSaxS <120261142+JuliaSaxS@users.noreply.github.com>
2023-06-13 21:18:29 +0300

    Added changes

--- Home_Work_Java/Task_2.java
+++ Home_Work_Java/Task_2.java
@@ -5,18 +5,17 @@ package Home_Work_Java;
 public class Task_2 {
     public static void main(String[] args) {
         int n = 2;
-
-        while(n<=1000){
+        while(n <= 1000) {
         int k = 1;
             long sum = 0;
-            while(k<=n-1){
+            while(k <= n - 1) {
                 long pow = 1;
                 for (int i = 0; i < n - 1; i++)
                     pow = (pow * k) % n;
-                sum = (sum + pow)%n;
+                sum = (sum + pow) % n;
                 k++;
             }
-            if(sum%n==n-1){
+            if(sum % n == n - 1) {
                 System.out.print(n + " ");
             }
             n++;
