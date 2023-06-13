ebc66cfa26328adba25dd5486a7435c79fe2872b
JuliaSaxS <120261142+JuliaSaxS@users.noreply.github.com>
2023-06-13 21:36:13 +0300

    Added Task 3.

---
+++ Home_Work_Java/Task_3.java
@@ -0,0 +1,30 @@
+package Home_Work_Java;
+import java.util.Scanner;
+public class Task_3 {
+    public static void main(String[] args) {
+        System.out.print("Введите первое число: ");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+        System.out.print("Введите второе число: ");
+        int numberOne = scan.nextInt();
+        System.out.print("Введите нужный знак (+, -, *, /): ");
+        String Symbol = scan.next();
+        System.out.print(" " + number + Symbol + numberOne);
+        switch (Symbol) {
+            case "+":
+                System.out.print(" = " + (number + numberOne));
+            break;
+            case "-":
+                System.out.print(" = " + (number - numberOne));
+            break;
+            case "*":
+                System.out.print(" = " + (number * numberOne));
+            break;
+            case "/":
+                System.out.print(" = " + (number / numberOne));
+            break;
+        }
+    scan.close();
+    }
+}
+
