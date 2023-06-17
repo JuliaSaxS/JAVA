d26fce964680387c6892b142c803712bd5a1c40e
JuliaSaxS <120261142+JuliaSaxS@users.noreply.github.com>
2023-06-17 20:03:23 +0300

    Added new Tasks

---
+++ Home_Work_Java/Task_5.java
@@ -0,0 +1,42 @@
+/* Дана строка sql-запроса "select * from students where ". 
+Сформируйте часть WHERE этого запроса, используя StringBuilder. 
+Данные для фильтрации приведены ниже в виде json-строки.
+Если значение null, то параметр не должен попадать в запрос.
+Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+*/
+
+public class Task_5 {
+    public static void main(String[] args) {
+      String sSQLQuery = "select * from students where";
+        StringBuilder sbSQLQuery = new StringBuilder();
+        sbSQLQuery.append(sSQLQuery);
+        String jsonString = "{\"name\":\"Ivanov\",\"country\":\"Russia\",\"city\":\"Moscow\",\"age\":\"null\"}";
+//        String jsonString = "{\"name\":\"Ivanov\"}";
+//        String jsonString = "{\"name\":\"Ivanov\",\"age\":\"null\"}";
+        jsonString = jsonString.replace("{", "");
+        jsonString = jsonString.replace("}", "");
+        if (jsonString.contains("\"")) {
+            jsonString = jsonString.replace("\"", "");
+        }
+        if (jsonString.contains("'")) {
+            jsonString = jsonString.replace("'", "");
+        }
+        String[] elements = jsonString.split(",");
+        for (String elem : elements) {
+            String[] pair = elem.split(":");
+            if (pair.length > 1) {
+                if (!pair[1].toLowerCase().equals("null")) {
+                    sbSQLQuery.append("\n");
+                    sbSQLQuery.append(pair[0]);
+                    sbSQLQuery.append(" = \"");
+                    sbSQLQuery.append(pair[1]);
+                    sbSQLQuery.append("\" and");
+                }
+            }
+        }
+        if (sbSQLQuery.substring(sbSQLQuery.length() - 3, sbSQLQuery.length()).equals("and")) {
+            sbSQLQuery.delete(sbSQLQuery.length() - 3, sbSQLQuery.length());
+        }
+        System.out.printf("SQL-запрос:\n%s", sbSQLQuery);
+    }
+}
---
+++ Home_Work_Java/Task_6.java
@@ -0,0 +1,65 @@
+//  Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+
+import java.io.IOException;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
+public class Task_6 {
+public static void main(String[] args) throws IOException {
+    Logger logger = Logger.getLogger(Task_6.class.getName());
+    FileHandler fh = new FileHandler("log.txt", false);
+    logger.addHandler(fh);
+    SimpleFormatter sFormat = new SimpleFormatter();
+    fh.setFormatter(sFormat);
+    byte[] arr = new byte[10];
+    StringBuilder sbArray = new StringBuilder();
+        sbArray.append("Исходный массив: ");
+        for (int i = 0; i < arr.length; i++) {
+            //  Math.random() - случайное число double из промежутка [0,1)
+            arr[i] = (byte) (Math.random() * 100);
+            sbArray.append(arr[i]);
+            sbArray.append(" ");
+        }
+        System.out.println(sbArray);
+        logger.log(Level.WARNING, sbArray.toString());
+        sbArray.delete(0, sbArray.length());
+        sbArray.append("Сортированный массив: ");
+        arr = BubbleSort(arr, logger);
+        for (byte b : arr) {
+            sbArray.append(b);
+            sbArray.append(" ");
+        }
+        System.out.println(sbArray);
+        logger.log(Level.WARNING, sbArray.toString());
+    }
+    public static byte[] BubbleSort(byte[] array, Logger logger) {
+        StringBuilder sbArray = new StringBuilder();
+        int iterCount = 0;
+        boolean isSorted = false;
+        byte temp;
+        while (!isSorted) {
+            isSorted = true;
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    isSorted = false;
+                    temp = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = temp;
+                }
+            }
+            iterCount++;
+            sbArray.append("Массив после ");
+            sbArray.append(iterCount);
+            sbArray.append(" итерации: ");
+            for (byte b : array) {
+                sbArray.append(b);
+                sbArray.append(" ");
+            }
+            logger.info(sbArray.toString());
+            sbArray.delete(0, sbArray.length());
+        }
+        return array;
+    }
+}
